b970c5257fe11dbd154ab0ec808468cd50344141
karixdev <kari.klucz@gmail.com>
2022-11-02 20:20:30 +0100

    feat: create folder dtos

---
+++ src/main/java/com/github/karixdev/notesapp/folder/validation/UniqueFolderNameValidator.java
@@ -0,0 +1,22 @@
+package com.github.karixdev.notesapp.folder.validation;
+
+import com.github.karixdev.notesapp.folder.FolderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+@Component
+@RequiredArgsConstructor
+public class UniqueFolderNameValidator implements ConstraintValidator<UniqueFolderName, String> {
+
+    private final FolderRepository folderRepository;
+
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+        return folderRepository
+                .findFolderByName(s)
+                .isEmpty();
+    }
+}
