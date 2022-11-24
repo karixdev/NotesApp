d303d10e3098b139cae5b02c18a6ada724c97416
karixdev <kari.klucz@gmail.com>
2022-11-24 01:54:58 +0100

    feat: create test for getById FolderService

--- src/test/java/com/github/karixdev/notesapp/folder/FolderServiceTest.java
+++ src/test/java/com/github/karixdev/notesapp/folder/FolderServiceTest.java
@@ -1,5 +1,6 @@
 package com.github.karixdev.notesapp.folder;
 
+import com.github.karixdev.notesapp.exception.ResourceNotFoundException;
 import com.github.karixdev.notesapp.folder.dto.FolderResponse;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -7,7 +8,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class FolderServiceTest {
@@ -17,6 +23,45 @@ public class FolderServiceTest {
 
     @Mock
     FolderRepository folderRepository;
+
+    @Test
+    void GivenNonExistingFolderId_WhenGetById_ThenThrowsException() {
+        // Given
+        Long id = 1L;
+
+        when(folderRepository.findById(any(Long.class)))
+                .thenReturn(Optional.empty());
+
+        // When & Then
+        assertThrows(ResourceNotFoundException.class,
+                () -> underTest.getById(id));
+    }
+
+    @Test
+    void GivenExistingFolderId_WhenGetById_ThenReturnsFolderResponse() {
+        // Given
+        Long id = 1L;
+
+        Folder folder = Folder.builder()
+                .id(1L)
+                .name("Folder")
+                .build();
+
+        when(folderRepository.findById(any(Long.class)))
+                .thenReturn(Optional.of(folder));
+
+        // When
+        FolderResponse result = underTest.getById(id);
+
+        // Then
+        FolderResponse expected = FolderResponse.builder()
+                .id(folder.getId())
+                .name(folder.getName())
+                .notes(folder.getNotes())
+                .build();
+
+        assertEquals(expected, result);
+    }
 
     @Test
     void GivenFolderEntity_WhenMapToFolderResponse_ThenReturnsCorrectDto() {
