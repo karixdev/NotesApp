a02e4291497005ab11d3978c18c777eaebe108b2
karixdev <kari.klucz@gmail.com>
2022-11-24 01:41:35 +0100

    feat: create test for mapFolderToFolderResponse in FolderService

---
+++ src/test/java/com/github/karixdev/notesapp/folder/FolderServiceTest.java
@@ -0,0 +1,45 @@
+package com.github.karixdev.notesapp.folder;
+
+import com.github.karixdev.notesapp.folder.dto.FolderResponse;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class FolderServiceTest {
+
+    @InjectMocks
+    FolderService underTest;
+
+    @Mock
+    FolderRepository folderRepository;
+
+    @Test
+    void GivenFolderEntity_WhenMapToFolderResponse_ThenReturnsCorrectDto() {
+        // Given
+        Folder folder = Folder.builder()
+                .id(1L)
+                .name("Folder")
+                .build();
+
+        // When
+        FolderResponse result = underTest.mapFolderToFolderResponse(folder);
+
+        // Then
+        FolderResponse expected = FolderResponse.builder()
+                .id(folder.getId())
+                .name(folder.getName())
+                .notes(folder.getNotes())
+                .build();
+
+        assertEquals(expected.getId(), result.getId());
+        assertEquals(expected.getName(), result.getName());
+        assertEquals(expected.getNotes(), result.getNotes());
+    }
+
+}
